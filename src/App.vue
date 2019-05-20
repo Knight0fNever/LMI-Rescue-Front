96934346f23812b41674b3d3c53aac7301152cc1
Josh Conner <jconner050612@gmail.com>
2019-05-19 22:23:52 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainLogo />
+    <Seats class="seats" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainLogo from "./components/MainLogo";
+import Seats from "./components/Seats";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    MainLogo,
+    Seats
+  },
+  mounted() {
+    var token = process.env.VUE_APP_TOKEN;
+    console.log(token);
+    var data = JSON.stringify({
+      TOKEN: "28c19520-f068-472a-a3c9-b76474af4788",
+      seat: "ithelp",
+      user: ""
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open("GET", "http://localhost:3000/seats");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Accept", "*/*");
+
+    xhr.send(data);
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.seats {
+  margin-top: 5em;
 }
 </style>
---
+++ src/components/MainLogo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="logo">
+    <img src="../assets/boxit_logo.svg" alt="boxIT Logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLogo'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .logo {
+    background-color: #362D7E;
+  }
+
+
+</style>
